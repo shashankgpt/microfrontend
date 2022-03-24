710ee187f9cca5db6e988b4254e27c4b1d5f7e8c
shashankgpt <shashankgpt270@gmail.com>
2022-03-24 16:52:58 +0530

    feat(app): trick shared dependency

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,6 +3,7 @@ const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -18,7 +19,7 @@ const devConfig = {
             remotes: {
                 marketing:'marketing@http://localhost:8081/remoteEntry.js'
             },
-            shared: ['react', 'react-dom']
+            shared: packageJSON.dependencies
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -3,6 +3,7 @@ const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJSON = require('../package.json');
 
 const devConfig = {
     mode: 'development',
@@ -19,7 +20,7 @@ const devConfig = {
             exposes: {
                 './MarketingApp':'./src/bootstrap',
             },
-            shared: ['react', 'react-dom']
+            shared: packageJSON.dependencies
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
